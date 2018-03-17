3070fafb1367672cda6ff9b439f75bbf66ff7e9a
Benjamin Atkin <ben@benatkin.com>
2018-03-17 18:59:57 -0400

    remove access code auth

--- test/browser.test.js
+++ test/browser.test.js
@@ -15,9 +15,7 @@ const nextBin = join(__dirname, '..', 'node_modules', '.bin', 'next')
 const cwd = join(__dirname, '..')
 const env = {
   ...process.env,
-  NODE_ENV: 'production',
-  SESSION_KEY: 'b5286b7ca136457924c4bb04bec328d2e6a74ff87cbb0d89667676c5432fed10055ac7d56195963060906977c4dddba35e3f8ac770c3ee7bac1f303ac4c450d8',
-  ACCESS_CODE: '53184f758a2ef399dddbb246f4b3f7fd'
+  NODE_ENV: 'production'
 }
 
 async function startApp() {
@@ -48,19 +46,11 @@ describe('Home', () => {
     }
   })
 
-  it('logs in', async () => {
+  it('opens the site', async () => {
+    await waitFor(200)
     await browser.get('http://localhost:3000/')
-    await waitFor(200)
+    await waitFor(800)
 
-    const input = await browser.elementByCss('input[name=accessCode]')
-    await input.sendKeys(env.ACCESS_CODE)
-
-    const submit = await browser.elementByCss('input[type=submit]')
-    const submitValue = await submit.getAttribute('value')
-    expect(submitValue).toBe('ENTER')
-    await submit.click()
-
-    await waitFor(200)
     const h1 = await browser.elementByCss('.section-bar')
     const headerText = await h1.text()
     expect(headerText).toMatch(/output/i)
